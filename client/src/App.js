db64e411d4718f532b89c16aabcffde2e29d104a
Anurag Raina <rainakalhan90@gmail.com>
2023-09-02 17:21:23 +0530

    Update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import Home from './Components/Home';
+import Details from './Components/CoinDetails/Graph';
+import Buy from './Components/Market';
 import './App.css';
 import { React, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginModal from './Components/LoginModal';
+import Dashboard from './Components/UserInformation/Dashboard';
 import Nav from './Components/Nav';
 import Signup from './Components/Signup';
+import UpdateInfo from './Components/UserInformation/UpdateInfo';
 import ProtectedBuyTransaction from './Components/Protected/ProtectedBuyTransaction';
 import ProtectedSellTransaction from './Components/Protected/ProtectedSellTransaction';
 
@@ -34,6 +38,10 @@ function App() {
 						path='/transactionSell'
 						element={<ProtectedSellTransaction open={[setOpen, setOpensign]} />}
 					/>
+					<Route exact path='/coin' element={<Details open={[setOpen, setOpensign]} />} />
+					<Route exact path='/market' element={<Buy />} />
+					<Route exact path='/dashboard' element={<Dashboard />} />
+					<Route exact path='/profileUpdate' element={<UpdateInfo />} />
 
 					{/* <Route exact path="/createUser" element={<LoginModal/>}/> */}
 				</Routes>
