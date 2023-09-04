a952c9cd4f7750dd5c8921f83b2427b391cb3088
Anurag Raina <rainakalhan90@gmail.com>
2023-09-04 18:00:00 +0530

    Update frontend and db urls

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { header } = require('express-validator');
 const dashboardRouter = require('./Routes/Dashboard');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-const PORT = 3001;
+app.use(
+	cors({
+		origin: 'https://crypto-markt.netlify.app',
+		credentials: true,
+	})
+);
 
-mongoose
-	.connect(Connection_url, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => app.listen(PORT, () => console.log(`runnging ${PORT}`)))
-	.catch((error) => console.log(error.message));
+//here are routes for backend calls
 
-mongoose.set('strictQuery', true);
-mongoose.set('strictQuery', true);
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', 'https://crypto-markt.netlify.app');
+	res.header(
+		'Access-Control-Allow-Origin',
+		'Origin,X-Requested-With,Content-Type,Accept',
+		'Access-Control-Allow-Methods: GET, DELETE, HEAD, OPTIONS, POST'
+	);
+	// header("Access-Control-Allow-Methods:POST,GET,OPTION,PUT,DELETE")
+	// header("Access-Control-Allow-Headers:Content-Type,X-Auth-Token,Origin,Authorization")
+	next();
+});
 
 app.use(express.json());
 app.use('/dashboard', dashboardRouter);
@@ -30,3 +41,9 @@ app.use('/register', require('./Routes/Signup'));
 app.use('/transactions', require('./Routes/Transactions'));
 app.use('/transactions', require('./Routes/Transactions'));
 app.use('/wallet', require('./Routes/Wallet'));
+
+mongoose.set('strictQuery', true);
+mongoose
+	.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/crypro_market')
+	.then(() => app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`)))
+	.catch((error) => console.error(error.message));
